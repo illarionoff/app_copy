e7b8c42d485aee8b490f9ba9e204dc2537cd75ff
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2019-04-29 06:46:24 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api/customers", (req, res) => {
+  const customers = [
+    {
+      id: 1,
+      name: "Alex",
+      lastname: "Lee"
+    }
+  ];
+  res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on PORT ${port}`);
+});
